88063e8092b0efbfc5712b3eafe21cfcaf5fe775
NathansStuff <n.odonnell@hotmail.com>
2022-11-24 21:54:15 +1100

    fix: fix path alias

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 module.exports = {
   webpack: {
-    alias: {},
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@features': path.resolve(__dirname, 'src/features/'),
+      '@data': path.resolve(__dirname, 'src/data/'),
+      '@context': path.resolve(__dirname, 'src/context/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+    },
   },
 };
